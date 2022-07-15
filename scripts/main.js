72f5ee7c9409a28a8dfc12f59918e600229f1c3d
CasLinden <cas_linden@msn.com>
2022-07-16 01:32:58 +0900

    added a separate inteface module, added a erase button to it to reset the board and gamestate

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const board = (function(){
 
-    let state = [null, null, null, null, null, null, null, null, null];
+    let state = [ null, null, null, 
+                  null, null, null, 
+                  null, null, null ];
 
     const allFields = () => {
         const container = document.querySelector("#board-container");
@@ -34,24 +36,13 @@ const board = (function(){
         let fields = allFields();
         for(let field of fields){
             field.addEventListener('click', consumeTurn, false);
-            function consumeTurn() {
-            let number = +this.getAttribute('data-index');
-            game.turn(number);
-            };
+                
+                function consumeTurn() {
+                let number = +this.getAttribute('data-index');
+                game.turn(number);
+                };
         };
     };
-
-    const signalTurn = (player) => {
-        const one = document.querySelector('#player1');
-        const two = document.querySelector('#player2');
-        if (player == player1){
-            one.style.border = '1px solid red';
-            two.style.border = 'none'
-        } if (player == player2){
-            two.style.border = '1px solid red';
-            one.style.border = 'none'
-        }
-    }
 
     const lightUp = (row) => {
         const squares = fields(row);
@@ -71,22 +62,47 @@ const board = (function(){
         }
     }
 
+    setClick();
+    lightsOff()
 
-return {state, render, setClick, field, clear, lightUp, fields, lightsOff, signalTurn};
+
+return {state, render, setClick, field, clear, lightUp, fields, lightsOff};
 })();
 
-board.setClick();
-board.lightsOff()
 
+/* --------------------------------------------------------------------------------*/
+const interface = (function(){
 
+    const eraser = () => {
+        const button = document.querySelector('#eraser');
+        button.addEventListener('click', erase, false); 
+                function erase() {
+                board.clear()
+                };
+    }
 
+    const signalTurn = (player) => {
+        const one = document.querySelector('#player1');
+        const two = document.querySelector('#player2');
+        if (player == player1){
+            one.style.color = 'red';
+            two.style.color = 'black'
+        } if (player == player2){
+            two.style.color = 'red';
+            one.style.color = 'black'
+        }
+    }
+    
+return {eraser, signalTurn}
+})();
+
+interface.eraser()
 /* --------------------------------------------------------------------------------*/
 
 const Player = (name, symbol) => {
     
     const play = (number) => {
             const playedField = board.field(number);
-            playedField.textContent = symbol;
             if (symbol === 'X'){
                 playedField.innerHTML = `
                 <svg  class='x' width="140" height="140" viewBox="0 0 222 217" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -94,13 +110,13 @@ const Player = (name, symbol) => {
                 <rect width="222" height="217" fill="white"/>
                 <path d="M171.069 28C128.976 81.7712 84.9104 132.532 37.1745 180.049C36.4008 180.819 38.5245 178.227 38.8635 177.134C42.1638 166.486 41.7563 153.547 41.6274 142.608C41.2428 109.964 36.6913 77.7381 31.3396 45.72C30.7156 41.9863 29.4509 37.2545 30.2648 33.316C30.6339 31.5295 30.9155 31.4108 32.7728 32.6872C44.7687 40.9316 56.5666 57.0368 66.1441 67.4412C95.8841 99.749 125.874 131.646 157.25 161.986C173.492 177.691 190.118 191.44 207 206" stroke="black" stroke-width="5" stroke-linecap="round"/>
                 </svg>`
-            }
+            };
             if (symbol === 'O'){
                 playedField.innerHTML =
                 `<svg class='o'width="120" height="120" viewBox="0 0 144 161" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M72 3C84.342 3 96.5836 7.82356 107 14.2222C114.999 19.136 121.772 24.694 125.778 33.5556C127.955 38.3729 132.024 41.6277 134.556 46.2222C137.454 51.4828 138.868 57.7067 140 63.5556C144.538 87.0028 135.811 110.958 121.333 129.222C108.854 144.965 88.8297 155.25 68.8889 157.778C55.2468 159.507 40.8527 149.932 30.4444 142.222C17.9763 132.987 11.3496 119.483 5.44445 105.444C0.465255 93.6071 2.92072 79.666 7 67.7778C10.8761 56.4817 13.4184 44.7747 19.2222 34.2222C23.5112 26.424 26.5398 21.2621 34.3333 17C40.364 13.702 46.8193 12.617 53.4444 11.2222C59.4155 9.96515 68.0063 10.3292 73 7" stroke="black" stroke-width="5" stroke-linecap="round"/>
                 </svg>`
-            }
+            };
             board.state[number] = symbol;
             console.log(board.state)
             };
@@ -112,17 +128,50 @@ const Player = (name, symbol) => {
 return {name, symbol, play, win};
 };
 
-const player1 = Player('player1', 'X');
-const player2 = Player('player2', 'O');
-board.signalTurn(player1)
+/* --------------------------------------------------------------------------------*/
+
+let player1 = Player('player1', 'X');
+let currentPlayer = player1
+
+
+
+/* --------------------------------------------------------------------------------*/
+const AI = (name, symbol) => {
+    const prototype = Player(name, symbol);
+
+    const legalMoves = () => {
+        const moves = [];
+        for(let i = 0; i <= 8; i++){
+            if(board.state[i] == null){
+               moves.push(i);
+               };
+            };
+        return moves;
+    };
+    
+    const randomMove = () => {
+        if(game.currentPlayer == player2){
+           let moves = legalMoves();
+           let choice = Math.floor(Math.random() * legalMoves().length);
+           game.turn(moves[choice]);
+           };
+    };
+    
+
+    const lmao = 'rofl'
+    
+    return Object.assign({}, prototype, {randomMove, lmao});
+};
+
+let player2 = AI('bot', 'O');
+
 
 /* --------------------------------------------------------------------------------*/
 
 const game = (function(){
-    let currentPlayer = player1;
 
     const changePlayer = () => {
-    currentPlayer == player1? currentPlayer = player2 : currentPlayer = player1;
+        currentPlayer === player1? currentPlayer = player2 : currentPlayer = player1;
     };
     
     const turn = (number) => {
@@ -132,7 +181,7 @@ const game = (function(){
         if(game.checkDraw()){board.clear()};
         checkWin()
         changePlayer();
-        board.signalTurn(currentPlayer)
+        interface.signalTurn(currentPlayer)
     };
 
     let state = {
